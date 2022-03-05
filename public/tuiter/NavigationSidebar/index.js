b5592a9b80cb5752a2dc059303bf7c6fb38a636b
Christian DeSimone <desimone.c@husky.neu.edu>
2022-03-04 20:21:54 -0500

    update todos links, add margin to bottom of explore posts

--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -5,28 +5,28 @@ const NavigationSidebar = () => {
                                 <i class="fa-brands fa-twitter"></i>
                             </a>
             <!-- continue the rest of the list -->
-            <a href="home.html" class="list-group-item list-group-item-action p-1">
+            <a href="../home.html" class="list-group-item list-group-item-action p-1">
                 <i class="fa-solid fa-house-chimney"></i><span class="d-xl-inline d-none">Home</span>
             </a>
                         <a href="explore.html" class="list-group-item list-group-item-action active p-1" aria-current="true">
                             <i class="fa-solid fa-hashtag"></i> <span class="d-xl-inline d-none">Explore</span>
                         </a>
-                        <a href="notifications.html" class="list-group-item list-group-item-action p-1">
+                        <a href="../notifications.html" class="list-group-item list-group-item-action p-1">
                             <i class="fa-solid fa-bell"></i> <span class="d-xl-inline d-none">Notifications</span>
                         </a>
-                        <a href="messages.html" class="list-group-item list-group-item-action p-1">
+                        <a href="../messages.html" class="list-group-item list-group-item-action p-1">
                             <i class="fa-solid fa-envelope"></i> <span class="d-xl-inline d-none">Messages</span>
                         </a>
-                        <a href="bookmarks.html" class="list-group-item list-group-item-action p-1">
+                        <a href="../bookmarks.html" class="list-group-item list-group-item-action p-1">
                             <i class="fa-solid fa-bookmark"></i> <span class="d-xl-inline d-none">Bookmarks</span>
                         </a>
-                        <a href="lists.html" class="list-group-item list-group-item-action p-1">
+                        <a href="../lists.html" class="list-group-item list-group-item-action p-1">
                             <i class="fa-solid fa-list-ul"></i> <span class="d-xl-inline d-none">Lists</span>
                         </a>
-                        <a href="profile.html" class="list-group-item list-group-item-action p-1">
+                        <a href="../profile.html" class="list-group-item list-group-item-action p-1">
                             <i class="fa-solid fa-user"></i> <span class="d-xl-inline d-none">Profile</span>
                         </a>
-                        <a href="more.html" class="list-group-item list-group-item-action p-1">
+                        <a href="../more.html" class="list-group-item list-group-item-action p-1">
                             <span class="fa-stack fa-2x wd-stack">
                                 <i class="fa-solid fa-circle fa-stack-2x fa-2x"></i>
                                 <i class="fa-solid fa-ellipsis-h fa-stack-2x text-black wd-hover-text-blue"></i>
--- public/tuiter/PostSummaryList/index.js
+++ public/tuiter/PostSummaryList/index.js
@@ -2,7 +2,7 @@ import PostSummaryListItem from "./PostSummaryListItem.js";
 import posts from "./posts.js"
 const PostSummaryList = () => {
     return (`
-            <ul class="list-group">
+            <ul class="list-group mt-2 mb-2">
             <!-- continue here -->
             ${posts.map(PostSummaryListItem).join("")}
             </ul>
